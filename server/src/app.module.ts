48c2542ce91359cbff97fbfc1031f925824de30a
MoloZzz <mega_oleksii@ukr.net>
2026-02-15 13:32:39 +0300

    chore: connect db, init modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './db/datasource';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+  TypeOrmModule.forRoot(dataSourceOptions),
+  OrdersModule,
+  UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/db/datasource.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
+
+  migrations: [__dirname + '/migrations/*.{ts,js}'],
+ssl:
+    process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  synchronize: false,
+  logging: process.env.DATABASE_LOGGING === 'true' ? true : false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export default new DataSource(dataSourceOptions);
