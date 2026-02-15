058dd05194e5f6aad7cbe8530b53496d1a189f39
MoloZzz <mega_oleksii@ukr.net>
2026-02-15 13:45:41 +0300

    chore: swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { Logger as PinoLogger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useLogger(app.get(PinoLogger));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // then validator will strip validated object of any properties that do not have any decorators (ValidatorOptions)
+      transform: true, // allow automatic transformation of incoming data (ValidationPipeOptions)
+      transformOptions: {
+        enableImplicitConversion: true, // enable transformation of data types (ClassTransformOptions)
+      },
+    }),
+  );
 
+  const configService = app.get(ConfigService);
+
+  if (configService.get<string>('API_DOCS_ENABLED') === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('API documentation')
+      .setDescription('Development API documentation ')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
